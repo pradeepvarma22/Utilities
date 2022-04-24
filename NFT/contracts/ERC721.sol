e2948a49d4c138123b684b46dab41355987637a4
pradeepvarma22 <pradeepnadimpalli22@gmail.com>
2022-04-24 16:49:27 +0530

    NFT: ownerOf
    Signed-off-by: pradeepvarma22 <pradeepnadimpalli22@gmail.com>

--- NFT/contracts/ERC721.sol
+++ NFT/contracts/ERC721.sol
@@ -6,6 +6,7 @@ pragma solidity ^0.8.0;
 contract ERC721 {
     //list of users having nft's
     mapping(address => uint256) internal _balances;
+    mapping(uint256 => address) internal _owners;
 
     //number of nfts assigned to owner
     function balanceOf(address owner) public view returns (uint256) {
@@ -14,10 +15,18 @@ contract ERC721 {
         return _balances[owner];
     }
 
+    //Finds the owner of an NFT where we give input as [Unique NFT id] like tokenId
+    //before calling this function we need a track who owns this NFT [_owner map]
+    function ownerOf(uint256 tokenId) public view returns (address) {
+        address owner = _owners[tokenId];
+        //documentation: check owner!= zero address:  if token is assigned to zero address it consider as invalid
+        require(owner != address(0), "TokenId does not exit");
+        return owner;
+    }
+
     //event Transfer()
     //event Approval()
     //event ApprovalForAll()
-    //function ownerOf()
     //function safeTransferFrom()
     //function safeTransferFrom()
     //function transferFrom()
